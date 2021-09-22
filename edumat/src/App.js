c3762d0c3a18c484891883356bad6c3881c82b39
Maria22 <m.ushakova22@gmail.com>
2021-09-22 22:21:31 +0200

    add fonts

--- edumat/src/App.js
+++ edumat/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <>
+      <h1>hkjfjkjkf</h1>
       <Header />
     </>
   );
--- edumat/src/globalStyles.js
+++ edumat/src/globalStyles.js
@@ -1,20 +1,31 @@
-import style, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import MetropolisWoff from './fonts/Metropolis-Regular.woff';
+import MetropolisWoff2 from './fonts/Metropolis-Regular.woff2';
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Metropolis';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Metropolis'), local('Metropolis'),
+  url(${MetropolisWoff2}) format('woff2'), 
+       url(${MetropolisWoff}) format('woff')
+}
+
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Metropolis';
 }
 `
 export const theme = {
     colors: {
         backgroundColor: '#FFFFFF',
         fontColor: '#C1C1C1',
-        darkestFontColor: 'darkestgrey',
+        darkestFontColor: '#7C7C7C',
         headersColor: '#131330',
         fieldColor: '#F4F4F4',
-        highlightColor: 'blackpearl',
         iconColor: '#3671FE'
     },
     media: {
